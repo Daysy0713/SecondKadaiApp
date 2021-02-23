fd23d4ddd79a103e7ce8eb142be3a9766d4d19a1
shogo iioka <shogosoccer0715@icloud.com>
2021-02-24 05:35:50 +0900

    Initial Commit

---
+++ SecondkadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondkadaiApp
+//
+//  Created by 飯岡祥吾 on 2021/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
